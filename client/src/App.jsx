0ccee4e690898f8726743612140719f0138f2240
ShahHetviIT <hetvi4551@gmail.com>
2023-12-15 22:00:30 +0530

    login basics

---
+++ client/src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'; // Import useState from React
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your form submission logic here
+    axios.post('http://localhost:3001/Login',{email,password})
+    .then(result => console.log(result))
+    .catch(err =>  console.log(err))
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
